807082bc7719b69b0f9ce7702527c3cfa39e830d
Micaela Uliczki <micauliczki@gmail.com>
2020-09-26 20:27:34 -0300

    Avance tp2

---
+++ TP2_ULICZKI/Entidades/Sedan.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+
+namespace Entidades
+{
+    public class Sedan : Vehiculo
+    {
+        public enum ETipo { CuatroPuertas, CincoPuertas }
+        private ETipo tipo;
+
+        /// <summary>
+        /// Por defecto, TIPO será Monovolumen
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="chasis"></param>
+        /// <param name="color"></param>
+        public Sedan(EMarca marca, string chasis, ConsoleColor color)
+            : base(chasis, marca, color)
+        {
+            this.tipo = ETipo.CuatroPuertas;
+        }
+
+        public Sedan(EMarca marca, string chasis, ConsoleColor color, ETipo tipo)
+           : this(marca, chasis, color)
+        {
+            this.tipo = tipo;
+        }
+
+
+        /// <summary>
+        /// Los automoviles son medianos
+        /// </summary>
+        protected override Vehiculo.ETamanio Tamanio
+        {
+            get
+            {
+                return Vehiculo.ETamanio.Mediano;
+            }
+        }
+
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("SEDAN");
+            sb.AppendLine(base.Mostrar());
+            sb.Append($"TAMAÑO : {this.Tamanio} ");
+            sb.AppendLine($"TIPO : {this.tipo}");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
