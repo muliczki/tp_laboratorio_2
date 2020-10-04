6e71bcbf73dc09af1505d90095f934aa955ccf95
Micaela Uliczki <micauliczki@gmail.com>
2020-10-04 12:06:16 -0300

    TP2 final - con diagrama de clases

--- TP2_ULICZKI/Entidades/Vehiculo.cs
+++ TP2_ULICZKI/Entidades/Vehiculo.cs
@@ -36,7 +36,7 @@ namespace Entidades
         /// <summary>
         /// ReadOnly: Retornará el tamaño
         /// </summary>
-        protected virtual ETamanio Tamanio { get; }
+        protected abstract ETamanio Tamanio { get; }
 
         /// <summary>
         /// Publica todos los datos del Vehiculo.
